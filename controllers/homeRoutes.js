fffb4a007bd49872a0e8e2ff29f6e6a5464f2f10
Nam Bui <nam.bui17@icloud.com>
2022-12-04 22:32:05 -0600

    Remove moment from routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render('homepage', {
       posts,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
